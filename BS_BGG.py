8924905b9086db3665ee4463a6bf63b734034fbd
Иван <e741@mail.ru>
2023-05-04 21:23:28 +0300

    clear code

--- BS_BGG.py
+++ BS_BGG.py
@@ -7,32 +7,15 @@ bot = telebot.TeleBot("5043606399:AAEQjRbRlYmEpvXPhzLQu1I7_3M1eJ6-s8w", parse_mo
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
-    bot.send_message(message.chat.id, "идет обработка запроса")
-
     req = requests.get('https://boardgamegeek.com/geeksearch.php?action=search&q='
                    + message.text + '&objecttype=boardgame')
     tree = BS(req.text, 'html.parser')
 
-    tags = tree.find_all('a')
-    ranks = []
+    tags = tree.find_all('a', attrs={'name': True})
     for tag in tags:
-        try:
-            rank = tag['name']
-            ranks.append(rank)
-        except:
-            pass
-
-    tags2 = tree.find_all(class_='primary')
-    names = []
-    for tag2 in tags2:
-        name = tag2.text
-        names.append(name)
-
-    for i in range(len(ranks)):
-        if int(ranks[i]) < 3000:
-            post = names[i] + ': ' + ranks[i]
-            bot.send_message(message.chat.id, post)
-    bot.send_message(message.chat.id, "конец")
+        name = tag.find_next(class_='primary')
+        post = name.text + ': ' + tag['name']
+        bot.send_message(message.chat.id, post)
 
 
 bot.polling(none_stop=True)
